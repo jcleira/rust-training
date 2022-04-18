fa553b0bf03aba9e5458a784e269ce7df0fe1ae0
jcleira <jmc.leira@gmail.com>
2022-04-18 07:59:51 +0200

    Add Rust Programming Language book match example

--- rust_book/enums/src/main.rs
+++ rust_book/enums/src/main.rs
@@ -10,6 +10,18 @@ enum IpAddress {
     V6(String),
 }
 
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
 fn main() {
     let v4 = IpVersion::V4;
     println!("Hello, {:?}", v4);
@@ -20,4 +32,38 @@ fn main() {
 
     let v6_address = IpAddress::V6(String::from("::1"));
     println!("Hello, {:?}", v6_address);
+
+    let quarter = Coin::Quarter(UsState::Alaska);
+
+    if let Coin::Quarter(us_state) = quarter {
+        println!("State quarter from {:?}!", us_state);
+    }
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let mut none = None;
+    none = plus_one(none);
+
+    println!("Number, {:?}", five);
+    println!("Number, {:?}", six);
+    println!("Number, {:?}", none);
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State {:?} quarter!", state);
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
 }
